3742703d15d4c565d4a0837f653d46fbe2795d5d
JQ <jesusq+github@gmail.com>
2022-06-13 17:34:56 -0700

    Initial files, worked on variables

---
+++ assets/js/1-variables.js
@@ -0,0 +1,31 @@
+console.log("-----variables.js-----")
+
+// 1.
+let varLet = "varByLet"
+const varConst = "varByConst"
+var varVar = "varByVar"
+
+// 2.
+console.log(`let: ${varLet}`)
+console.log(`const: ${varConst}`)
+console.log(`var: ${varVar}`)
+
+// 3.
+varLet = "changedByLet"
+varVar = "changedByVar"
+
+console.log(`let: ${varLet}`)
+console.log(`var: ${varVar}`)
+
+// 4.
+varConst = "changedByConst"
+
+console.log(`const: ${varConst}`)
+
+// A: an error is thrown -- Uncaught TypeError: Assignment to constant variable.
+
+// 5.
+// A: Use const to assign values that you know will never or should not change
+
+// 6.
+// Don't use var to declare - because it doesn't have block scope
