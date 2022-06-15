8e849d08e039dd15b3fed3164c7780a9a7555b5a
JQ <jesusq+github@gmail.com>
2022-06-15 15:45:06 -0700

    Worked on Numbers

---
+++ assets/js/2-numbers.js
@@ -0,0 +1,24 @@
+console.log("-----numbers.js-----")
+
+// 1.
+let myNum = 23;
+console.log(myNum);
+
+// 2.
+myNum += 15;
+console.log(myNum);
+
+// 3.
+myNum -= 8;
+console.log(myNum);
+
+// 4.
+const myAvg = (26 + 44 + 72 + 86 + 112) / 5;
+console.log(myAvg);
+
+// 5.
+let theNumber = 14;
+console.log(++theNumber);
+
+// 6.
+console.log(typeof myNum);
